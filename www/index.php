a39f3f3f4b3ebeb4cf698644589363c2e3aec4a6
mark <mark@coder.com.tw>
2021-02-08 10:44:30 +0800

    init

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<?php
+phpinfo();
+// $servername = "localhost";
+// $username = "root";
+// $password = "admin";
+// $dbname = "docker_test";
+//   try {
+//     $db = new PDO(
+//        'mysql:host=mysql;dbname=docker_test;port=3306;charset=utf8',$username,$password);
+// }catch(PDOException $e) {
+//     // echo 'DB connection failed';
+//     echo $e->getMessage();
+//    exit;
+// }
+// $sql = "select * from member";
+// $result = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($result);exit;
+
+?>
